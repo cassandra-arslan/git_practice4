cbe507421163342d0ed475258e14556025e637c6
Cassandra Arslan <naimaarslan@gmail.com>
2022-01-24 08:21:49 -0800

    Added comments

--- src/Hello/TestCase002.java
+++ src/Hello/TestCase002.java
@@ -3,5 +3,8 @@ package Hello;
 public class TestCase002 {
     public static void main(String[] args) {
         System.out.println("Just practicing");
+
+        //checking to see if I can commit, push, and delete branches
+
     }
 }
